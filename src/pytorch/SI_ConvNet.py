9477614d900d8861aac8dd31590e3251d1888df3
Rocky <ruoqi.zhang.666@gmail.com>
2019-12-19 14:04:39 +0100

    fix the bugs in SI_ConvNet but still has one.: F.conv2d() for same weight

--- src/pytorch/SI_ConvNet.py
+++ src/pytorch/SI_ConvNet.py
@@ -18,25 +18,49 @@ class ScaleInvariance_Layer(nn.Module):
 		self.in_channels = in_channels
 		self.out_channels = out_channels
 		self.kernel_size = kernel_size
+		# print(stride)
 		self.stride = stride
 		self.padding = padding
 		self.scale_range = scale_range
+		self.dilation = dilation
+		self.convs = []
+		# default groups = 1
+		self.bias = None
+		self.weight = Parameter(torch.Tensor(
+                in_channels, out_channels // 1, kernel_size[0]))
+		self.reset_parameters()
+
+	def reset_parameters(self):
+        init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in)
+            init.uniform_(self.bias, -bound, bound)
 
 
 	def scale(self, input):
 		upsamples = []
-		for s in range(len(scale_range)):
+		# print(self.scale_range)
+		for s in self.scale_range:
 			# upsampling
-			ups = F.upsample_bilinear(input, scale_factor=s)
-			upsamples.append(ups)
+			# print(s)
+			ups = nn.Upsample(scale_factor=s, mode='bilinear')
+			s = ups(input)
+			upsamples.append(s)
 		return upsamples
 
 	def forward(self, input):
 		outputs = []
 		self.upsamples = self.scale(input)
-		for i in range(len(upsamples)):
-			padding = int((self.kernel_size-1)/2)
-			out = F.conv2d(input, self.upsamples[i], None, self.stride, padding, self.dilation)
+		for i in range(len(self.upsamples)):
+			# note: here think the filter is n*n
+			padding = int((self.kernel_size[0]-1)/2)
+			# F.conv2d(F.pad(input, expanded_padding, mode='circular'),
+   #                          weight, self.bias, self.stride,
+   #                          _pair(0), self.dilation, self.groups)
+   # input, weight, bias=None, stride=1, padding=0, dilation=1,
+			print(self.stride)
+			out = F.conv2d(self.upsamples[i].unsqueeze_(0), weight=self.weight, bias=None, stride=self.stride, padding=padding, dilation=self.dilation)
 			# Undo scaling
 			outputs.append(out.unsqueeze(-1))
 
@@ -44,12 +68,12 @@ class ScaleInvariance_Layer(nn.Module):
 		return F.relu(strength)
 
 
-class Net_scaleinvariant_mnist_scale(object):
+class Net_scaleinvariant_mnist_scale(nn.Module):
 	"""docstring for Net_scaleinvariant_mnist_scale"""
 	def __init__(self):
 		super(Net_scaleinvariant_mnist_scale, self).__init__()
 		
-		kernel_size = [11,11,11]
+		kernel_sizes = [11,11,11]
 		pads = (np.array(kernel_sizes) - 1) / 2
 		pads = pads.astype(int)
 
