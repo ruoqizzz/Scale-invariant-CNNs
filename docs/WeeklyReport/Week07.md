df32734c3fb8c53a72e11a5efa21747087185c8c
Rocky <ruoqi.zhang.666@gmail.com>
2019-12-29 14:41:29 +0100

    update weekly report

---
+++ docs/WeeklyReport/Week07.md
@@ -0,0 +1,9 @@
+# Weekly Report 07
+
+## Things we did this week
+
+- Implement Anti-aliased SS-CNN which integrating classical antialiasing with SS-CNN. More details are shown in `Implementation.pdf` and `Making-CNN-ShiftInvaraiant-Again-Summary.pdf`
+- Rewrite SI-ConvNet using Pytorch and evaluation
+
+Note: All the implementation and evaluation details are in the `Implementation.pdf`  except caffe version of SI-ConvNet.
+
--- src/pytorch/Antialiased_SSCNN.py
+++ src/pytorch/Antialiased_SSCNN.py
@@ -179,12 +179,12 @@ class Net_antialiased_steerinvariant_mnist_scale(nn.Module):
         # self.pool1 = nn.MaxPool2d(2)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=1)
         # Typically, blur kernel M is 3 or 5
-        self.down1 = Downsample(channels=lays[0], filt_size=5, stride=2)
+        self.down1 = Downsample(channels=lays[0], filt_size=3, stride=2)
         self.bn1 = nn.BatchNorm2d(lays[0])
 
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=1)
         # Typically, blur kernel M is 3 or 5
-        self.down2 = Downsample(channels=lays[1], filt_size=5, stride=2)
+        self.down2 = Downsample(channels=lays[1], filt_size=3, stride=2)
         self.bn2 = nn.BatchNorm2d(lays[1])
 
         self.pool3 = nn.MaxPool2d(kernel_size=8, stride=1, padding=2)
